05a7f5cec8b48c640d8bdf6f7a94cff59ee64ced
Jack Cho <jack06215@gmail.com>
2023-02-18 22:32:41 +0800

    minor fix

--- src/main/kotlin/com/example/backend/domain/customer/Customer.kt
+++ src/main/kotlin/com/example/backend/domain/customer/Customer.kt
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Customer(
     @JsonProperty("id") val id: Long,
-    @JsonProperty("first_name") val firstName: String,
-    @JsonProperty("last_ame") val lastName: String,
+    @JsonProperty("firstName") val firstName: String,
+    @JsonProperty("lastName") val lastName: String,
 )
